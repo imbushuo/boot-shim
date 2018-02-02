a007f7887b0ced4bd45ad19f28426d8981f2870a
Bingxing Wang <i@imbushuo.net>
2018-02-01 23:14:17 -0500

    Switch to boot application mode.

---
+++ include/application.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// Sup types
+#include "suptypes.h"
+
+// Bootloader
+#include "bl.h"
+
+// Status enum
+#include "ntstatus.h"
--- src/BlApplicationEntry.c
+++ src/BlApplicationEntry.c
@@ -1,11 +1,20 @@
-/*
- * UEFI:SIMPLE - UEFI development made easy
- * Copyright © 2014-2017 Pete Batard <pete@akeo.ie> - Public Domain
- * See COPYING for the full licensing terms.
- */
 #include <efi.h>
 #include <efilib.h>
 
+#include "application.h"
+
+NTSTATUS BlApplicationEntry(
+	_In_ PBOOT_APPLICATION_PARAMETER_BLOCK BootAppParameters,
+	_In_ PBL_LIBRARY_PARAMETERS LibraryParameters
+)
+{
+
+	// Do nothing right now
+	return STATUS_SUCCESS;
+
+}
+
+// This is entry stub.
 // Application entrypoint (must be set to 'efi_main' for gnu-efi crt0 compatibility)
 EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 {
@@ -16,14 +25,14 @@ EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 #endif
 
 	/*
-	 * In addition to the standard %-based flags, Print() supports the following:
-	 *   %N       Set output attribute to normal
-	 *   %H       Set output attribute to highlight
-	 *   %E       Set output attribute to error
-	 *   %B       Set output attribute to blue color
-	 *   %V       Set output attribute to green color
-	 *   %r       Human readable version of a status code
-	 */
+	* In addition to the standard %-based flags, Print() supports the following:
+	*   %N       Set output attribute to normal
+	*   %H       Set output attribute to highlight
+	*   %E       Set output attribute to error
+	*   %B       Set output attribute to blue color
+	*   %V       Set output attribute to green color
+	*   %r       Human readable version of a status code
+	*/
 	Print(L"\n%H*** UEFI:SIMPLE ***%N\n\n");
 
 	Print(L"%EPress any key to exit.%N\n");
