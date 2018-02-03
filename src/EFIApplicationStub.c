f03dd6423c6a746ec2b7546977a73b00d1918f75
Bingxing Wang <i@imbushuo.net>
2018-02-03 11:53:17 -0500

    Removed timer.

--- src/EFIApplicationStub.c
+++ src/EFIApplicationStub.c
@@ -5,26 +5,96 @@
 // Application entrypoint (must be set to 'efi_main' for gnu-efi crt0 compatibility)
 EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 {
-	UINTN Event;
+
+	UINTN NumHandles;
+	EFI_STATUS status = EFI_SUCCESS;
+	EFI_HANDLE *SFS_Handles;
+	EFI_BLOCK_IO_PROTOCOL *BlkIo;
+	EFI_DEVICE_PATH_PROTOCOL *FilePath;
+	EFI_LOADED_IMAGE_PROTOCOL *ImageInfo;
+	EFI_HANDLE AppImageHandle = NULL;
+	CHAR16 *FileName = L"Menu.efi";
+	UINTN ExitDataSize;
 
 #if defined(_GNU_EFI)
 	InitializeLib(ImageHandle, SystemTable);
 #endif
 
-	/*
-	* In addition to the standard %-based flags, Print() supports the following:
-	*   %N       Set output attribute to normal
-	*   %H       Set output attribute to highlight
-	*   %E       Set output attribute to error
-	*   %B       Set output attribute to blue color
-	*   %V       Set output attribute to green color
-	*   %r       Human readable version of a status code
-	*/
-	Print(L"\n%H*** ~ Never gonna give up up ~ ***%N\n");
-	Print(L"*** ~ Never gonna let you down ~ ***\n\n");
+	Print(L"*** Attempt to load Menu.efi. ***\n\n");
+	Print(L"*** Locating handle buffers. ***\n\n");
 
-	Print(L"%E This device will restart in 30 seconds. %N\n");
-	SystemTable->BootServices->Stall(30000000);
+	status = gBS->LocateHandleBuffer(
+		ByProtocol,
+		&gEfiSimpleFileSystemProtocolGuid,
+		NULL,
+		&NumHandles,
+		&SFS_Handles);
 
-	return EFI_SUCCESS;
+	if (status != EFI_SUCCESS) 
+	{
+		Print(L"Could not find handles - %r\n", status);
+		goto exit;
+	}
+
+	Print(L"*** Attempting to open protocols.. ***\n\n");
+	for (UINTN index = 0; index < NumHandles; index++) 
+	{
+		status = gBS->OpenProtocol(
+			SFS_Handles[index],
+			&gEfiSimpleFileSystemProtocolGuid,
+			(VOID**)&BlkIo,
+			ImageHandle,
+			NULL,
+			EFI_OPEN_PROTOCOL_GET_PROTOCOL);
+
+		if (status != EFI_SUCCESS) {
+			Print(L"Protocol is not supported - %r\n", status);
+			goto exit;
+		}
+
+		FilePath = FileDevicePath(SFS_Handles[index], FileName);
+		status = gBS->LoadImage(
+			FALSE,
+			ImageHandle,
+			FilePath,
+			(VOID*)NULL,
+			0,
+			&AppImageHandle);
+
+		if (status != EFI_SUCCESS) {
+			Print(L"Could not load the image - %r\n", status);
+			continue;
+		}
+
+		Print(L"Loaded the image with success\n");
+		status = gBS->OpenProtocol(
+			AppImageHandle,
+			&gEfiLoadedImageProtocolGuid,
+			(VOID**) &ImageInfo,
+			ImageHandle,
+			(VOID*)NULL,
+			EFI_OPEN_PROTOCOL_GET_PROTOCOL
+		);
+
+		Print(L"ImageInfo opened\n");
+
+		if (!EFI_ERROR(status)) {
+			Print(L"ImageSize = %d\n", ImageInfo->ImageSize);
+		}
+
+		Print(L"Image start:\n");
+		status = gBS->StartImage(AppImageHandle, &ExitDataSize, (CHAR16**) NULL);
+		if (status != EFI_SUCCESS) {
+			Print(L"Could not start the image - %r %x\n", status, status);
+			Print(L"Exit data size: %d\n", ExitDataSize);
+			continue;
+		}
+
+	}
+
+exit:
+	Print(L"%E Will exit in 10 seconds. %N\n");
+	SystemTable->BootServices->Stall(10000000);
+	return status;
+
 }
