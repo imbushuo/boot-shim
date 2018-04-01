2e79618df4bf8e02a1c44a3b64c5363d3ee4bd5c
Bingxing Wang <i@imbushuo.net>
2018-03-31 19:48:35 -0700

    Generalize naming (replace LK to Payload)

--- include/EFIApp.h
+++ include/EFIApp.h
@@ -14,12 +14,7 @@
 #define SECONDS_TO_MICROSECONDS(x) x * 1000000
 #endif
 
-ElfVerifierStatus CheckElf32Header(Elf32_Ehdr* header);
-ElfVerifierStatus CheckElf64Header(Elf64_Ehdr* header);
-
-VOID Arm32JumpToAddress(EFI_HANDLE ImageHandle, uint32_t addr);
-
-EFI_STATUS Elf32Load(EFI_HANDLE ImageHandle, VOID* PayloadFileBuffer);
-EFI_STATUS Elf64Load(EFI_HANDLE ImageHandle, VOID* PayloadFileBuffer);
+BOOLEAN CheckElf32Header(Elf32_Ehdr* header);
+VOID JumpToAddress(EFI_HANDLE ImageHandle, uint32_t addr);
 
 #endif
